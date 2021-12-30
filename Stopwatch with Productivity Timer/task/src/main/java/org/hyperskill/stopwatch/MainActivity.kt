a6e128ba5b2a6ab7e2e8cc47ff6fb10202048e1e
Zhandos <200103398@stu.sdu.edu.kz>
2021-12-30 19:11:39 +0600

    Stage3

--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
@@ -1,20 +1,32 @@
 package org.hyperskill.stopwatch
 
 import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.DrawableWrapper
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     val list = mutableListOf(0, 0)
     val handler = Handler(Looper.getMainLooper())
     var isAlive = false
 
-    var textView: TextView? = null
+
+    lateinit var progressBar:ProgressBar
+    lateinit var textView: TextView
+
+
     val task: Runnable = object : Runnable {
         override fun run() {
             textView?.text = convert(list[0]) + ":" + convert(list[1])
@@ -26,34 +38,54 @@ class MainActivity : AppCompatActivity() {
             handler.postDelayed(this, 1000)
         }
     }
+    var changeColor = false
 
-    @SuppressLint("SetTextI18n")
+    val progressTask: Runnable = object : Runnable {
+        override fun run() {
+            if (changeColor) {
+                progressBar.indeterminateTintList = ColorStateList.valueOf(Color.parseColor("#4169E1"))
+            }
+            else {
+                progressBar.indeterminateTintList = ColorStateList.valueOf(Color.parseColor("#F44336"));
+            }
+            changeColor = !changeColor
+            handler.postDelayed(this, 1000)
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         textView = findViewById(R.id.textView)
+        progressBar = findViewById(R.id.progressBar)
+        progressBar.visibility = View.GONE
+
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
             if (!isAlive) {
+                progressBar.visibility = View.VISIBLE
                 list[1] += 1
                 handler.postDelayed(task, 1000)
+                handler.postDelayed(progressTask, 1000)
                 isAlive = true
             }
         }
 
         findViewById<Button>(R.id.resetButton).setOnClickListener {
             if (isAlive) {
+                progressBar.visibility = View.GONE
                 list.fill(0)
+                handler.removeCallbacks(progressTask)
                 handler.removeCallbacks(task)
-                textView?.text = "00:00"
+                textView.text = "00:00"
                 isAlive = false
             }
         }
     }
-
-
 
     fun convert(value: Int):String {
         return if (value < 10) {
