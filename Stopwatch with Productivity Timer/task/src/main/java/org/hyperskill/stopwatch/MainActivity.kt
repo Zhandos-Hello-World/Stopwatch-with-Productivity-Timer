5b076cf6d120c0e03e03c99f1c7f04f7ac162ff2
Zhandos <200103398@stu.sdu.edu.kz>
2022-01-01 18:23:14 +0600

    Stage 4

--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
@@ -1,35 +1,49 @@
 package org.hyperskill.stopwatch
 
-import android.annotation.SuppressLint
+
+import android.app.AlertDialog
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.DrawableWrapper
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val list = mutableListOf(0, 0)
     val handler = Handler(Looper.getMainLooper())
     var isAlive = false
+    var limit = 0
+    var limitIsAlive = false
 
 
     lateinit var progressBar:ProgressBar
     lateinit var textView: TextView
+    lateinit var settings: Button
 
 
     val task: Runnable = object : Runnable {
         override fun run() {
-            textView?.text = convert(list[0]) + ":" + convert(list[1])
+            if (limitIsAlive) {
+                if (list[0] * 60 + list[1] > limit) {
+                    textView.setTextColor(Color.RED)
+                }
+                else {
+                    textView.setTextColor(Color.GRAY)
+                }
+            }
+            else {
+                textView.setTextColor(Color.GRAY)
+            }
+
+            textView.text = convert(list[0]) + ":" + convert(list[1])
             list[1]++
             if (list[1] == 60) {
                 list[1] = 0;
@@ -54,13 +68,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         textView = findViewById(R.id.textView)
+        textView.setTextColor(Color.GRAY)
+
         progressBar = findViewById(R.id.progressBar)
         progressBar.visibility = View.GONE
 
@@ -72,6 +87,7 @@ class MainActivity : AppCompatActivity() {
                 handler.postDelayed(task, 1000)
                 handler.postDelayed(progressTask, 1000)
                 isAlive = true
+                settings.isEnabled = false
             }
         }
 
@@ -82,8 +98,38 @@ class MainActivity : AppCompatActivity() {
                 handler.removeCallbacks(progressTask)
                 handler.removeCallbacks(task)
                 textView.text = "00:00"
+                textView.setTextColor(Color.GRAY)
                 isAlive = false
+                limitIsAlive = false
+                settings.isEnabled = true
             }
+        }
+
+        val contentView = LayoutInflater.from(this).inflate(R.layout.dialog_main, null, false)
+        val edit_text = contentView.findViewById<EditText>(R.id.upperLimitEditText)
+        val alert = AlertDialog.Builder(this)
+            .setTitle("Set upper limit in seconds")
+            .setView(contentView)
+            .setPositiveButton("OK") { _, _ ->
+                if (edit_text.text.toString().isNotEmpty()) {
+                    limit = edit_text.text.toString().toInt()
+                    limitIsAlive = true
+                }
+                else {
+                    limitIsAlive = false
+                }
+                edit_text.setText("")
+            }
+            .setNegativeButton("Cancel") { _, _ ->
+                edit_text.setText("")
+            }
+            .create()
+
+
+        settings = findViewById(R.id.settingsButton)
+
+        settings.setOnClickListener {
+                alert.show()
         }
     }
 
